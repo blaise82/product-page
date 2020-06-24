c05c4fa3e4193d85f5e0e4b7de40f5f44cc2ffec
Izabayo Blaise <52036624+blaise82@users.noreply.github.com>
2020-06-24 12:32:58 +0200

    reload

--- src/App.js
+++ src/App.js
@@ -1,47 +1,33 @@
-import React from "react";
-import svg1 from "./svg1.svg";
-import "./App.css";
+import React, { Component, useState } from "react";
 
-function App() {
+
+const Dummy = () => {
+  const inputRef = useRef();
+
+  const [name, setName] = useState("");
+  const [lastName, setLastName] = useState("");
+
   return (
-    <div className="App">
-      <header>
-        <div className="left">
-          <div className="logo">
-            <span className="I">I</span>tegure
-          </div>
-        </div>
-        <div className="right">
-          <div className="buttons">
-            <button class="btn">signup</button>
-            <button class="btn1">sign in</button>
-          </div>
-        </div>
-      </header>
-
-      <div>
-        <div className="half">
-        <div class="squircle"></div>
-          <div className="left">
-            <div className="detail-wrap">
-              <p className="head"><span className="color1">*</span>801<span className="color2">*</span>123<span className="color3">#</span></p>
-              <p className="subHead">Welcome to Itegure || Leaning Reminder</p>
-              <p className="detail">
-                We send information on schedules of courses that your children
-                will be learning on Radio and TV on time in this covid-19
-                period.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="half center">
-          <div className="right">
-            <img src={svg1} className="svg1" alt="logo" />
-          </div>
-        </div>
-      </div>
-    </div>
+    <>
+      <input ref={inputRef} name="name" value={name} placeholder="name" />
+      <input
+        ref={inputRef}
+        name="lastName"
+        value={lastName}
+        placeholder="lastName"
+      />
+      <button type="button" onClick={() => inputRef.current.focus()}>
+        Button
+      </button>
+    </>
   );
+};
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Dummy />
+      </div>
+    );
+  }
 }
-
-export default App;
